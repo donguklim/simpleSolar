53fae2e9a2416a10e460049c09dd05c38cf87b83
dlimdlim <dlim7272@gmail.com>
2022-09-28 03:30:03 +0900

    load a model

--- simple_solar_system/main.cpp
+++ simple_solar_system/main.cpp
@@ -150,7 +150,7 @@ int main(int argc, char** argv)
   helloVk.initGUI(0);  // Using sub-pass 0
 
   // Creation of the example
-  helloVk.loadModel(nvh::findFile("media/scenes/cube_multi.obj", defaultSearchPaths, true));
+  helloVk.loadModel(nvh::findFile("model/earth/geometry.obj", defaultSearchPaths, true));
 
   helloVk.createOffscreenRender();
   helloVk.createDescriptorSetLayout();
